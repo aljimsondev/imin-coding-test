b53631becf20a564715226157732f3ded7ab93fa
aljimsondev <aljimson.megrino@gmail.com>
2025-10-24 02:29:47 +0800

    Chore | added footer color and header dummy content

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,18 @@
-import Typography from '@/components/typography';
+import IconButton from '@/components/button/icon.button';
+import Image from 'next/image';
+import { BsCart2 } from 'react-icons/bs';
+import { IoPersonOutline } from 'react-icons/io5';
 import './header.css';
 
 function Header() {
   return (
     <nav className="header">
       <div className="container header-content">
-        <Typography>Brand Name</Typography>
+        <Image src="/assets/logo/logo.webp" alt="logo" width={70} height={70} />
+        <div className="header-right-content">
+          <IconButton icon={BsCart2} iconProps={{ size: 24 }} />
+          <IconButton icon={IoPersonOutline} iconProps={{ size: 24 }} />
+        </div>
       </div>
     </nav>
   );
